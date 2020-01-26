fc93347ef59bfef5b672205581b1684962f43ac5
George <admin@georgesimos.com>
2020-01-26 12:16:17 +0200

    Updates

---
+++ precache-manifest.6d025e093d5abcc14719d018b9d59ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e1f4821f75c8b389afce8697f1f683",
+    "url": "/paper-and-ink/index.html"
+  },
+  {
+    "revision": "f04f9ac5586eb09daf64",
+    "url": "/paper-and-ink/static/css/main.f6dd6d59.chunk.css"
+  },
+  {
+    "revision": "874e3a87760e2d27ef3f",
+    "url": "/paper-and-ink/static/js/2.847d2bd4.chunk.js"
+  },
+  {
+    "revision": "fda7bfcf097202ec3f45ffc763119d3e",
+    "url": "/paper-and-ink/static/js/2.847d2bd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f04f9ac5586eb09daf64",
+    "url": "/paper-and-ink/static/js/main.b84d39cd.chunk.js"
+  },
+  {
+    "revision": "dcceaffa5de108da3a05",
+    "url": "/paper-and-ink/static/js/runtime-main.73601bfa.js"
+  }
+]);
